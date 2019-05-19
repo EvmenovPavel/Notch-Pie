6314645ecc7ef8ede106822828cf9f1822c6bd14
oddlyspaced <oddlyspaced1@gmail.com>
2019-05-19 12:45:06 +0530

    Edit Constant File

---
+++ app/src/main/java/com/oddlyspaced/np/Constants/ConstantHolder.java
@@ -0,0 +1,33 @@
+package com.oddlyspaced.np.Constants;
+
+import android.os.Environment;
+
+// Sample file holding constant data for app
+public class ConstantHolder {
+
+    private final int appCode = 7;
+    private final String configFolderPathInternal = Environment.getExternalStorageDirectory() + "/Android/data/com.oddlyspaced.np";
+    private final String configFilePathInternal = configFolderPathInternal + "/config";
+    private final String configFolderPathExternal = Environment.getExternalStorageDirectory() + "/NotchPie";
+    private final String configFetchUrl = "https://raw.githubusercontent.com/oddlyspaced/NotchPie-Data/master/";
+
+    public int getAppCode() {
+        return appCode;
+    }
+
+    public String getConfigFolderPathInternal() {
+        return configFolderPathInternal;
+    }
+
+    public String getConfigFilePathInternal() {
+        return configFilePathInternal;
+    }
+
+    public String getConfigFolderPathExternal() {
+        return configFolderPathExternal;
+    }
+
+    public String getConfigFetchUrl() {
+        return configFetchUrl;
+    }
+}
