e7f54c089b3f800e906e42182a16e4ffa36cea81
oddlyspaced <oddlyspaced1@gmail.com>
2019-05-19 08:31:00 +0530

    Added defined fragment for Notch Settings

--- app/src/main/java/com/oddlyspaced/np/Fragments/NotchSettingsFragment.java
+++ app/src/main/java/com/oddlyspaced/np/Fragments/NotchSettingsFragment.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,7 +15,13 @@ public class NotchSettingsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View main = inflater.inflate(R.layout.fragment_notch_config, container, false);
+        View main = inflater.inflate(R.layout.fragment_notch_settings, container, false);
         return  main;
+    }
+
+    // takes the parent view and attaches the listeners to it
+    private View init(View main) {
+
+        return main;
     }
 }
