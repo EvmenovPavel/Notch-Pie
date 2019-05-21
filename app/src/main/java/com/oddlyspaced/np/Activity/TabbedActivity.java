6e9b79382d8350dd730ca38a960bea27dbdecc9a
oddlyspaced <oddlyspaced1@gmail.com>
2019-05-22 00:43:51 +0530

    Create data folder in main

--- app/src/main/java/com/oddlyspaced/np/Activity/TabbedActivity.java
+++ app/src/main/java/com/oddlyspaced/np/Activity/TabbedActivity.java
@@ -6,8 +6,11 @@ import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
-import com.oddlyspaced.np.Adapter.SectionsPageAdapter;
+import com.oddlyspaced.np.Constants.ConstantHolder;
+import com.oddlyspaced.np.SectionsPageAdapter;
 import com.oddlyspaced.np.R;
+
+import java.io.File;
 
 public class TabbedActivity extends AppCompatActivity {
 
@@ -19,6 +22,7 @@ public class TabbedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabbed);
+        createDirectories();
         init();
     }
 
@@ -27,6 +31,12 @@ public class TabbedActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(new SectionsPageAdapter(getSupportFragmentManager(), titles));
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void createDirectories() {
+        File folder = new File(new ConstantHolder().getConfigFolderPathInternal());
+        if (!folder.exists())
+        folder.mkdirs();
     }
 
 }
