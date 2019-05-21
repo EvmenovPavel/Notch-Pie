1d2a2b65bebe3acfed964a84f6b0ea7c817cdccd
oddlyspaced <oddlyspaced1@gmail.com>
2019-05-22 00:48:15 +0530

    Make battery settings screen function

--- app/src/main/java/com/oddlyspaced/np/Fragments/BatterySettingsFragment.java
+++ app/src/main/java/com/oddlyspaced/np/Fragments/BatterySettingsFragment.java
@@ -1,27 +1,108 @@
 package com.oddlyspaced.np.Fragments;
 
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.oddlyspaced.np.BatteryColorAdapter;
+import com.oddlyspaced.np.Interface.ColorPickerListener;
+import com.oddlyspaced.np.Interface.OnTouchColorLevel;
+import com.oddlyspaced.np.Modal.ColorLevel;
 import com.oddlyspaced.np.R;
+import com.oddlyspaced.np.Utils.BatteryConfigManager;
+import com.oddlyspaced.np.Utils.ColorPicker;
+
+import java.util.ArrayList;
 
 public class BatterySettingsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View main = inflater.inflate(R.layout.fragment_battery_settings, container, false);
+        init();
+        main = attach(main);
         return  main;
     }
 
+    private RecyclerView recyclerView;
+    private BatteryColorAdapter batteryColorAdapter;
+    private ArrayList<ColorLevel> list;
+    private BatteryConfigManager manager;
+    private int positionTouch = -1;
+
+    private void init() {
+        manager = new BatteryConfigManager();
+    }
+
     // takes the parent view and attaches the listeners to it
-    private View init(View main) {
+    private View attach(final View main) {
+        final CheckBox linearAnimation = main.findViewById(R.id.cbLinearAnimation);
+        final CheckBox definedAnimation = main.findViewById(R.id.cbDefinedAnimation);
+
+        if (manager.read()) {
+            linearAnimation.setChecked(manager.isLinear());
+            linearAnimation.setChecked(manager.isDefined());
+        }
+
+        linearAnimation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                manager.setLinear(isChecked);
+                definedAnimation.setChecked(!isChecked);
+                manager.setDefined(!isChecked);
+                manager.save();
+            }
+        });
+        definedAnimation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                manager.setDefined(isChecked);
+                linearAnimation.setChecked(!isChecked);
+                manager.setDefined(!isChecked);
+                manager.save();
+            }
+        });
+
+        recyclerView = main.findViewById(R.id.rvBatteryLevel);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        list = manager.getColorLevels();
+        if (getContext() == null) {
+            Log.e("why", "why");
+        }
+        batteryColorAdapter = new BatteryColorAdapter(getContext(), list, new OnTouchColorLevel() {
+            @Override
+            public void onTouchItem(int position) {
+                positionTouch = position;
+                ColorPicker colorPicker = new ColorPicker(new ColorPickerListener() {
+                    @Override
+                    public void onColorSet(String color) {
+                        ColorLevel prev = list.get(positionTouch);
+                        prev.setColor(color);
+                        list.set(positionTouch, prev);
+                        batteryColorAdapter.notifyItemChanged(positionTouch);
+                        manager.setColorLevels(list);
+                        manager.save();
+                    }
+                });
+                colorPicker.color = list.get(position).getColor();
+                colorPicker.show(getFragmentManager(), "color picker");
+            }
+        });
+        recyclerView.setAdapter(batteryColorAdapter);
 
         return main;
     }
+
 }
---
+++ app/src/main/java/com/oddlyspaced/np/Utils/ColorPicker.java
@@ -0,0 +1,175 @@
+package com.oddlyspaced.np.Utils;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import com.oddlyspaced.np.Interface.ColorPickerListener;
+import com.oddlyspaced.np.R;
+
+// https://www.youtube.com/watch?v=ARezg1D9Zd0
+// Fragment class for ColorPicker dialog
+public class ColorPicker extends AppCompatDialogFragment {
+
+    // UI widgets
+    private SeekBar red;
+    private SeekBar green;
+    private SeekBar blue;
+    // the listener for handling color picker changes
+    public ColorPickerListener listener;
+    // the color preview in the dialog
+    private View dColor;
+    // text box for showing the color code
+    private EditText editTextColor;
+    // internal variable to store generated color
+    public String color;
+
+
+    public ColorPicker(ColorPickerListener listener) {
+        this.listener = listener;
+    }
+
+    // on creation of fragment
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    // on loading up of dialog
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        // load the layout to show in dialog
+        View view = inflater.inflate(R.layout.dialog_color_picker, null);
+        builder.setView(view); // setting view
+        builder.setCancelable(false);
+        builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                listener.onColorSet(color);
+            }
+        });
+        // initialising items
+        dColor = view.findViewById(R.id.viewDialogColor);
+        red = view.findViewById(R.id.sbRed);
+        red.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateColorString();
+                updateColorView(color);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        green = view.findViewById(R.id.sbGreen);
+        green.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateColorString();
+                updateColorView(color);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        blue = view.findViewById(R.id.sbBlue);
+        blue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateColorString();
+                updateColorView(color);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        editTextColor = view.findViewById(R.id.editTextColor);
+        editTextColor.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    dColor.setBackgroundColor(Color.parseColor(s.toString()));
+                    color = s.toString();
+                    updateColorSeekbar(color);
+                } catch (Exception e) {
+                    Toast.makeText(getContext(), "Invalid Color!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        updateColorSeekbar(color);
+        // return the created builder dialog
+        return builder.create();
+    }
+
+    // generate color string from the progress values
+    private void updateColorString() {
+        String r = Integer.toHexString(red.getProgress());
+        if (r.length() == 1)
+            r = "0" + r;
+        String g = Integer.toHexString(green.getProgress());
+        if (g.length() == 1)
+            g = "0" + g;
+        String b = Integer.toHexString(blue.getProgress());
+        if (b.length() == 1)
+            b = "0" + b;
+        color = "#" + r + g + b;
+    }
+
+    // update the color view
+    private void updateColorView(String c) {
+        dColor.setBackgroundColor(Color.parseColor(c));
+        editTextColor.setText(c);
+    }
+
+    // update values of seekbars
+    private void updateColorSeekbar(String c) {
+        red.setProgress(Integer.parseInt(c.substring(1, 3), 16));
+        green.setProgress(Integer.parseInt(c.substring(3, 5), 16));
+        blue.setProgress(Integer.parseInt(c.substring(5), 16));
+    }
+}
